80551daf4765ea04affdc3abd18e8fd4dd07cf27
alfikr <awal.baru77@gmail.com>
2020-05-17 14:16:24 +0700

    tested on registry

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const swaggerDoc = require('../swagger.json')
 /**
  * Loading environment variable
  */
-dotenv.config({path:__dirname+'/./../.env'})
+dotenv.config({ path: __dirname + '/./../.env' })
 dotenv.config({ path: __dirname + '/./../.env.dev' })
 const options = {
     swaggerOptions: {
@@ -48,15 +48,26 @@ app.use(compression())
  * eureka registration
  */
 const client = require('./config/eureka')
+client.start(error => {
+    console.log(error || 'erueka client started')
+    app.use(route)
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc, options))
+    app.get('/health', (req, res) => {
 
-app.use(route)
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc, options))
-app.get('/health', (req, res) => {
-     
-    console.log("Health json is called ");
-    res.json({"status":"UP"});
-  
-  });  
+        console.log("Health json is called ");
+        res.json({ "status": "UP" });
+
+    });
+    // javaInstance = client.getInstanceByAppId('NODE-SERVICE')
+    // const javaUrl = `${javaInstance[0].hostName}:${javaInstance[0].port.$}/service-instances/${javaInstance[0].app}`
+    // app.get(`/serviceInfo/${javaUrl}`,(req,res)=>{
+    //     res.send(JSON.stringify(javaInstance),null,2);
+    //     res.end()
+    // })
+})
+process.on('exit',()=>{
+    client.stop();
+})
 if (process.env.MESSAGE_BROKER == 'kafka') {
     console.log('loading kafka config')
 } else {
@@ -68,7 +79,7 @@ if (process.env.NODE_ENV == 'development') {
         logSnippet: false,
         watch: true,
         ui: false,
-        open:'local',
+        open: 'local',
         port: parseInt(process.env.PORT) + 1,
         proxy: 'localhost:' + process.env.PORT
     })
@@ -83,6 +94,6 @@ app.listen(app.get('port'), () => {
     console.log(process.env.PROJECT)
     console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'),
         app.get('env'));
-    
+
     console.log('tekan CTRL-C untuk berhenti')
 })
--- src/config/eureka.js
+++ src/config/eureka.js
@@ -4,6 +4,7 @@ const Eureka = require('eureka-js-client').Eureka;
 const client = new Eureka({
     instance:{
         app: process.env.PROJECT,
+        instanceId:`${process.env.PROJECT}-${Math.round(Math.random()*(9999-1000+1)+1000)}`,
         hostName: process.env.EUREKA_HOST,
         requestMiddleware:(requestOpts,done)=>{
             requestOpts.auth={
@@ -12,7 +13,9 @@ const client = new Eureka({
             }
             done(requestOpts)
         },
-        ipAddr: '127.0.0.1',
+        requestRetryDelay:60,
+        registyFetchInterval:10,
+        ipAddr: process.env.HOST,
         port: {
             $:process.env.PORT,
             '@enabled':'true'
@@ -26,7 +29,7 @@ const client = new Eureka({
         fetchRegistry:true,
     },
     eureka:{
-        host: process.env.EUREKA_HOST,
+        host: `${process.env.EUREKA_USERNAME}:${process.env.EUREKA_PASSWORD}@${process.env.EUREKA_HOST}`,
         port: process.env.EUREKA_PORT,
         serviceUrls:{
             'defaultZone':['http://admin:admin@localhost:8761/eureka/']
@@ -35,7 +38,4 @@ const client = new Eureka({
     }
 })
 client.logger.level('debug')
-client.start(error=>{
-    console.log(error || 'erueka client started')
-})
-exports.module=client
+module.exports=client
